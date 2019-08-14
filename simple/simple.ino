f58321b6741ffee0df1f8398e082f2557cf383ac
jsavage00 <savageapplications@gmail.com>
2019-08-15 00:28:18 +0100

    Update simple.ino

--- simple/simple.ino
+++ simple/simple.ino
@@ -1,15 +1,38 @@
 #include "MPU9250.h"
+#include "eeprom_utils.h"
 
 MPU9250 mpu;
 
+
+int ledPin = 13; // choose the pin for the LED
+int inPin = 8;   // choose the input pin (for a pushbutton)
+int val = 0;     // variable for reading the pin status
+
+
+void calibrate() {
+   mpu.setup();
+  mpu.setMagneticDeclination(-0.2);
+  mpu.calibrateAccelGyro();
+  mpu.calibrateMag();
+
+  // save to eeprom
+  saveCalibration();
+
+  // load from eeprom
+  loadCalibration();
+}
+
+
 void setup()
 {
-    Serial.begin(115200);
+  
+  Serial.begin(115200);
 
-    Wire.begin();
-
-    delay(2000);
-    mpu.setup();
+  Wire.begin();
+  mpu.setup();
+  //calibrate();
+  delay(2000);
+    
 }
 
 void loop()
@@ -17,17 +40,16 @@ void loop()
     static uint32_t prev_ms = millis();
     if ((millis() - prev_ms) > 16)
     {
-        mpu.update();
-        mpu.print();
+      mpu.update();
+      //mpu.print();
 
-        Serial.print("roll  (x-forward (north)) : ");
-        Serial.println(mpu.getRoll());
-        Serial.print("pitch (y-right (east))    : ");
-        Serial.println(mpu.getPitch());
-        Serial.print("yaw   (z-down (down))     : ");
-        Serial.println(mpu.getYaw());
 
-        prev_ms = millis();
+      Serial.print(mpu.getRoll());
+      Serial.print("|");
+      Serial.print(mpu.getPitch());
+      Serial.print("|");
+      Serial.println(mpu.getYaw());
+
+      prev_ms = millis();
     }
 }
-
